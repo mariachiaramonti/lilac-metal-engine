be954b0f01682a1accdaadbd6a6ee7a39a263811
Maria Chiara Monti <mariamonti96@gmail.com>
2025-09-02 21:35:23 +0100

    Rendering a red cube

--- Chapter3/Pipeline/Pipeline/Renderer.swift
+++ Chapter3/Pipeline/Pipeline/Renderer.swift
@@ -17,6 +17,42 @@ class Renderer : NSObject {
         Self.device = device
         Self.commandQueue = commandQueue
         metalView.device = device
+        
+        // create a mesh
+        let allocator = MTKMeshBufferAllocator(device: device)
+        let size: Float = 0.8
+        let mdlMesh = MDLMesh(
+            boxWithExtent: [size, size, size],
+            segments: [1, 1, 1],
+            inwardNormals: false,
+            geometryType: .triangles,
+            allocator: allocator)
+        do {
+            mesh = try MTKMesh(mesh: mdlMesh, device: device)
+        } catch {
+            print(error.localizedDescription)
+        }
+        vertexBuffer = mesh.vertexBuffers[0].buffer
+            
+        // create the shader function library
+        let library = device.makeDefaultLibrary()
+        Self.library = library
+        let vertexFunction = library?.makeFunction(name: "vertex_main")
+        let fragmentFunction = library?.makeFunction(name: "fragment_main")
+        
+        // create the pipeline state object
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
+        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlMesh.vertexDescriptor)
+        do {
+            pipelineState =
+            try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        
         super.init()
         
         metalView.clearColor = MTLClearColor (
@@ -40,6 +76,30 @@ extension Renderer: MTKViewDelegate {
     
     // Called every frame. Write render code here.
     func draw(in view: MTKView){
-        print("draw")
+        guard
+            let commandBuffer = Self.commandQueue.makeCommandBuffer(),
+            let descriptor = view.currentRenderPassDescriptor,
+            let renderEncoder =
+                commandBuffer.makeRenderCommandEncoder(
+                    descriptor: descriptor) else {
+            return
+        }
+        
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        for submesh in mesh.submeshes {
+            renderEncoder.drawIndexedPrimitives(
+                type: .triangle,
+                indexCount: submesh.indexCount,
+                indexType: submesh.indexType,
+                indexBuffer: submesh.indexBuffer.buffer,
+                indexBufferOffset: submesh.indexBuffer.offset)
+        }
+        renderEncoder.endEncoding()
+        guard let drawable = view.currentDrawable else {
+            return
+        }
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
     }
 }
